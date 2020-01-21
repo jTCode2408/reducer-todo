578e9ba40232ae2931eb50694db72260d7e86b40
Jasmine Terry <jasmine.terry91@gmail.com>
2020-01-21 16:09:43 -0500

    initial CRA setup/setup of reducer file

---
+++ reducer-todo/src/reducers/TodoReducer.js
@@ -0,0 +1,10 @@
+export const initialState = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+}
+
+export const TodoReducer = (state, action) =>{
+
+    return state
+}
